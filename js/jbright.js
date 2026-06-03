785f0c4772a59c808da5a1291ec425115254195b
Khem Visal <khemvisal@Khems-MacBook-Pro.local>
2026-06-03 08:52:17 +0700

    undo jbright.js

--- js/jbright.js
+++ js/jbright.js
@@ -23,8 +23,8 @@ window.JBright = {
             // Send paymentData to native iOS via WKWebView bridge
             if (window.webkit && window.webkit.messageHandlers && window.webkit.messageHandlers.jbright) {
                 window.webkit.messageHandlers.jbright.postMessage({
-                    action: method,
-                    data: payload
+                    action: action,
+                    data: data
                 });
             }
 
